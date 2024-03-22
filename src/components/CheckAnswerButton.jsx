55479afa464a84367f3cc857b270c42077364d29
Oğuzcan Toptaş <oguzcantoptas@gmail.com>
2024-03-22 20:22:36 +0300

    first commit

---
+++ src/components/CheckAnswerButton.jsx
@@ -0,0 +1,7 @@
+export default function CheckAnswerButton({ disabled }) {
+  return (
+    <button className="check-answer-button" type="submit" disabled={disabled}>
+      <span>Kontrol Et</span>
+    </button>
+  )
+}
---
+++ src/components/StartButton.jsx
@@ -0,0 +1,7 @@
+export default function StartButton({ clickHandler }) {
+  return (
+    <button className="start-button" type="button" onClick={clickHandler}>
+      <span>Başla</span>
+    </button>
+  )
+}
